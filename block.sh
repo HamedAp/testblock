2296965530d98b67a6d99efff5755aa796860529
Hamed Ap <109583222+HamedAp@users.noreply.github.com>
2023-12-15 22:25:06 +0330

    Update block.sh

--- block.sh
+++ block.sh
@@ -4,4 +4,4 @@ apt install ipset iptables -y
 
 bash <(curl -Ls https://raw.githubusercontent.com/HamedAp/testblock/main/shahanblock --ipv4)
 
-sudo iptables -A INPUT -j DROP
+sudo iptables -A INPUT -p tcp --dport 80 -j DROP
